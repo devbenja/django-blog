9a3323e2db1a488c419e267cd0750c2e9d16d61f
benjadev <cariasbenjamin59@gmail.com>
2023-09-05 10:08:07 -0700

    Ver detalles de una publicacion

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from .models import POST
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+# Vista para mostrar todos los post
 
 def post_list(request):
     posts = POST.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+# Vista para ver un detalle de un post 
+
+def post_detail(request, pk):
+    post = get_object_or_404(POST, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
